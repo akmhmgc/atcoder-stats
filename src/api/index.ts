dbd5146a34a3cd88abb86d13bfa447ee66878112
akmhmgc <akimuhamaguchi@icloud.com>
2022-08-11 23:58:34 +0900

    Add api response

---
+++ src/api/index.ts
@@ -0,0 +1,25 @@
+import { timeParse } from 'd3';
+import { scores } from '../fetchers/history';
+import dayjs from 'dayjs';
+const d3nLine = require('d3node-linechart');
+
+export default async (req: any, res: any) => {
+  res.setHeader("Content-Type", "image/svg+xml");
+
+  const {
+    username
+  } = req.query;
+  const myScores: any = await scores(username);
+  const data = myScores.map((score: any) => {
+    const parseTime = timeParse("%Y-%m-%d");
+    return {
+      key: parseTime(dayjs(score.EndTime).format('YYYY-MM-DD')),
+      value: score.NewRating,
+    }
+  }
+  )
+
+  const line = d3nLine({ data: data, isCurve: false });
+
+  return res.send(line.svgString());
+}
